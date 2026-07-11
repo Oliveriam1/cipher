54c19253a9291b6e10d1d7b1da09eaef36987c4e
Oliveriam <oliveriam1234@gmail.com>
2026-07-11 15:01:04 +0200

    Adding decryption for caesar cipher

--- main.c
+++ main.c
@@ -33,6 +33,39 @@ char* encrypt_caesar_cipher (char * text) {
     return text;
 }
 
+char* decrypt_caesar_cipher (char * text) {
+    for (int i = 0; text[i] != '\0' ; i++) {
+        if (text[i] == 'A' || text[i] == 'a') {
+            if (text[i] == 'a') {
+                text[i] = 'x';
+            }
+            else {
+               text[i] = 'X';
+            }
+        }
+        else if (text[i] == 'B' || text[i] == 'b') {
+            if (text[i] == 'b') {
+                text[i] = 'y';
+            }
+            else {
+               text[i] = 'Y';
+            }
+        }
+        else if (text[i] == 'C' || text[i] == 'c') {
+            if (text[i] == 'c') {
+                text[i] = 'z';
+            }
+            else {
+               text[i] = 'Z';
+            }
+        }
+        else {
+            text[i] = text[i] - 3;
+        }
+    }
+    return text;
+}
+
 int main () {
     int choice = 0;
     char text[1024] = "";
@@ -48,6 +81,9 @@ int main () {
             encrypt_caesar_cipher(text);
             printf(text);
             break;
+        case 2:
+            decrypt_caesar_cipher(text);
+            printf(text);
             //future cipher types coming soon...
     }
 }
